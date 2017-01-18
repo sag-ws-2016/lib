fe09982eec1ea414f2da97c5e59fcc3af4475593
ccaps <christophcaps@gmail.com>
2017-01-18 14:53:22 +0100

    Adds classes Vehicle, Taxi and Service

---
+++ main/src/model/Vehicle.java
@@ -0,0 +1,70 @@
+package model;
+
+/**
+ * Created by Christoph on 18.01.2017.
+ */
+public class Vehicle {
+    protected String id;
+    protected String numberPlate;
+    protected Person owner;
+    protected String insuranceNumber;
+    protected String insurance;
+    protected String brand;
+    protected String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+    public void setNumberPlate(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public String getInsuranceNumber() {
+        return insuranceNumber;
+    }
+
+    public void setInsuranceNumber(String insuranceNumber) {
+        this.insuranceNumber = insuranceNumber;
+    }
+
+    public String getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(String insurance) {
+        this.insurance = insurance;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
