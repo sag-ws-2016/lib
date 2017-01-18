aba0e1bf3037baf09981bb5aa540d67b606bcd66
Jonas Faber <dev.jonas.faber@gmail.com>
2017-01-18 13:29:52 +0100

    Add methods to Address

--- main/src/model/Address.java
+++ main/src/model/Address.java
@@ -5,6 +5,55 @@ package model;
  */
 public class Address {
     String street;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCareOf() {
+        return careOf;
+    }
+
+    public void setCareOf(String careOf) {
+        this.careOf = careOf;
+    }
+
     String houseNumber;
     String city;
     String zip;
