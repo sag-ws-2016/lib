99304761c700a10d787a884c96b6a81ab0ce5203
Jonas Faber <dev.jonas.faber@gmail.com>
2017-04-10 18:40:55 +0200

    Lombok Test.

--- src/main/java/com/sw/thm/model/Address.java
+++ src/main/java/com/sw/thm/model/Address.java
@@ -1,5 +1,6 @@
 package com.sw.thm.model;
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.io.Serializable;
@@ -11,6 +12,7 @@ import javax.persistence.*;
  */
 @Entity
 @Table( name = "ADDRESS" )
+@Data
 public class Address implements Serializable{
     @Id
     @GeneratedValue(generator = "uuid")
@@ -22,74 +24,5 @@ public class Address implements Serializable{
     protected String zip;
     protected String country;
     protected String careOf;
-
-    public Address() {
-    }
-
-    public Address(String street, String houseNumber, String city, String zip, String country, String careOf, String id) {
-        this.street = street;
-        this.houseNumber = houseNumber;
-        this.city = city;
-        this.zip = zip;
-        this.country = country;
-        this.careOf = careOf;
-        this.id = id;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCareOf() {
-        return careOf;
-    }
-
-    public void setCareOf(String careOf) {
-        this.careOf = careOf;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
 }
