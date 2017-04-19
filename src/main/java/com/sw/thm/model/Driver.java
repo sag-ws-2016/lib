42d35a00a4a96e4cd43a2b9f370e569eef1990c8
Fabian Beliza <fabian.beliza@iem.thm.de>
2017-04-19 13:42:13 +0200

    Add JsonAutoDetect Annotations

--- src/main/java/com/sw/thm/model/Driver.java
+++ src/main/java/com/sw/thm/model/Driver.java
@@ -1,8 +1,10 @@
 package com.sw.thm.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Data;
 import java.util.Date;
 @Data
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.PROTECTED_AND_PUBLIC)
 public class Driver extends Person {
     protected String nationality;
     protected String socialSecurityNumber;
