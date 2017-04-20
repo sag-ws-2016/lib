924aa3e81c66a7c00f74f305d0c5e10575f095b1
Jonas Faber <dev.jonas.faber@gmail.com>
2017-04-20 23:16:20 +0200

    Adds URI-Helper to Utils.

---
+++ src/main/java/com/sw/thm/util/Env.java
@@ -0,0 +1,37 @@
+package com.sw.thm.util;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jonas on 20.04.17.
+ */
+
+public final class Env {
+    public static Map<String, Object> getFormattedOverwrites() {
+        Map<String, String> env = System.getenv();
+        Map<String, Object> configOverrides = new HashMap<>();
+        for (String envName : env.keySet()) {
+            if (envName.contains("DATABASE_URL")) {
+                try {
+                    URI uri = new URI(env.get(envName));
+                    String[] userInfo = uri.getUserInfo().split(":");
+                    String user = userInfo[0];
+                    String password = userInfo[1];
+                    String url = "jdbc:" + uri.getScheme() + "://"
+                            + uri.getHost() + ":" + uri.getPort() + uri.getRawPath()
+                            + "?" + uri.getRawQuery();
+                    configOverrides.put("javax.persistence.jdbc.user", user);
+                    configOverrides.put("javax.persistence.jdbc.password", password);
+                    configOverrides.put("javax.persistence.jdbc.url", url);
+
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return configOverrides;
+    }
+}
