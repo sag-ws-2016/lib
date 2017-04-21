75d4609b4ee26fc4d69dc0764d4d6cf2589dc0e1
Jonas Faber <dev.jonas.faber@gmail.com>
2017-04-21 22:12:13 +0200

    Add Contracts Entity Classes.

--- src/main/java/com/sw/thm/model/Contract.java
+++ src/main/java/com/sw/thm/model/Contract.java
@@ -3,13 +3,19 @@ package com.sw.thm.model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-
+@Entity
+@Table(name = "CONTRACT")
 @Data
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.PROTECTED_AND_PUBLIC)
 public class Contract implements Serializable{
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     protected String id;
     protected double earnings;
     protected int hoursOfWork;
@@ -18,10 +24,11 @@ public class Contract implements Serializable{
     protected Date endDate;
 
     @JsonIgnore
+    @Transient
     public boolean isTemporary() {
         if (endDate == null) {
             return false;
-        }else {
+        } else {
             return true;
         }
     }
