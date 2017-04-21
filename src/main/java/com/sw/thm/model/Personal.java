520de278b648f37e98de8b72c530ca5f63acc94e
Jonas Faber <dev.jonas.faber@gmail.com>
2017-04-22 01:06:39 +0200

    Add Driver constructor to Personal.

--- src/main/java/com/sw/thm/model/Personal.java
+++ src/main/java/com/sw/thm/model/Personal.java
@@ -5,4 +5,15 @@ import lombok.Data;
 @Data
 public class Personal extends Person {
     protected String type;
+
+    public Personal(Driver driver) {
+        type = "DRIVER";
+        id = driver.getId();
+        firstName = driver.getFirstName();
+        lastName = driver.getLastName();
+        address = driver.getAddress();
+        phoneNumber = driver.getPhoneNumber();
+        placeOfBirth = driver.getPlaceOfBirth();
+        dateOfBirth = driver.getPlaceOfBirth();
+    }
 }
